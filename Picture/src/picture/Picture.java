2a75df1592168dc63398885ab6f04812e88f5cbf
Kurt <Kurt@10.0.1.15>
2016-01-31 14:02:11 -0600

    updating assignment details - see project files

---
+++ Picture/src/picture/Picture.java
@@ -0,0 +1,16 @@
+package picture;
+
+/**
+ *
+ * @author Kurt
+ */
+public class Picture {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
